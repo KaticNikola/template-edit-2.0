c04b46c0deacc9f55bf90c867a74637ecd7171d4
KaticNikola <nikolakatic1@gmail.com>
2019-11-26 11:16:50 +0100

    game
    Signed-off-by: KaticNikola <nikolakatic1@gmail.com>

---
+++ src/games/quiz/Quiz.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+
+import 'Quiz.scss'
+
+export class quiz extends Component {
+    render() {
+        return (
+            <div>
+                quiz
+            </div>
+        )
+    }
+}
+
+export default quiz
